5b20bb0d5b3c7abfdda71d830f6472594f9ecd9b
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2017-04-14 05:26:08 +0700

    Add AR indicator

---
+++ Assets/Scripts/ARIndicator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ARIndicator : MonoBehaviour
+{
+    public GameObject Indicator;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    void OnMarkerLost(ARMarker marker)
+    {
+        Indicator.SetActive(false);
+    }
+
+    void OnMarkerFound(ARMarker marker)
+    {
+        Indicator.SetActive(true);
+    }
+
+    void OnMarkerTracked(ARMarker marker)
+    {
+        Indicator.SetActive(true);
+    }
+}

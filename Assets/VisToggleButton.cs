a3bcc0d44ec54aec492f9c11e168e47e30737f6d
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2017-04-13 23:16:59 +0700

    Create crude visibility toggle button

---
+++ Assets/VisToggleButton.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VisToggleButton : MonoBehaviour {
+    public GameObject Target;
+    public string TextOnVisible;
+    public string TextOnInvisible;
+
+    private Button _button;
+    private Text _text;
+
+	void Start () 
+    {
+        _button = gameObject.GetComponent<Button>();
+        _text = _button.GetComponentInChildren<Text>();
+
+        _button.onClick.AddListener(this.ButtonClicked);
+	}
+	
+    void Update()
+    {
+    }
+
+    void OnGUI()
+    {
+        if (Target != null)
+        {
+            if (Target.activeSelf)
+            {
+                _text.text = TextOnVisible;
+            }
+            else
+            {
+                _text.text = TextOnInvisible;
+            }
+        }
+    }
+
+    void ButtonClicked()
+    {
+        if (Target != null)
+        {
+            if (Target.activeSelf)
+            {
+                Target.SetActive(false);
+            }
+            else
+            {
+                Target.SetActive(true);
+            }
+        }
+    }
+}

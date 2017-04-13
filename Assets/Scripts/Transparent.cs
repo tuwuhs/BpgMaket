bc15179d4286b4402da3d7c46d4a81317aacf0ed
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2017-04-14 01:22:13 +0700

    Create Transparent script

---
+++ Assets/Scripts/Transparent.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transparent : MonoBehaviour 
+{
+    public float Transparency = 1.0f;
+
+    private Material _material = null;
+    private Material _oldMaterial = null;
+
+    void Start()
+    {
+    }
+
+	void Update() 
+    {
+        if (Transparency < 1.0f)
+        {
+            if (_material == null)
+            {
+                _material = GetComponent<Renderer>().material;
+                _oldMaterial = new Material(_material);
+
+                // Change rendering mode to transparent
+                _material.SetFloat("_Mode", 3.0f);
+                _material.SetInt("_SrcBlend",
+                    (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                _material.SetInt("_DstBlend",
+                    (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                _material.SetInt("_ZWrite", 0);
+                _material.DisableKeyword("_ALPHATEST_ON");
+                _material.EnableKeyword("_ALPHABLEND_ON");
+                _material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                _material.renderQueue = 3000;
+            }
+
+            Color c = _material.color;
+            c.a = Transparency;
+            _material.color = c;
+        }
+        else
+        {
+            // Reset the material and remove this script
+            _material.CopyPropertiesFromMaterial(_oldMaterial);            
+            Destroy(this);
+        }
+	}
+}
---
+++ Assets/Scripts/TransparentChildren.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TransparentChildren : MonoBehaviour
+{
+    private float _transparency = 1.0f;
+    private List<GameObject> _children;
+
+    public float Transparency
+    {
+        get { return _transparency; }
+        set
+        {
+            _transparency = value;
+            foreach (GameObject obj in _children)
+            {
+                Renderer r = obj.GetComponent<Renderer>();
+                if (r == null) continue;
+
+                Transparent t = r.GetComponent<Transparent>();
+                if (t == null && _transparency < 1.0f)
+                {
+                    t = r.gameObject.AddComponent<Transparent>();
+                }
+
+                if (t != null)
+                {
+                    t.Transparency = _transparency;
+                }
+            }
+        }
+    }
+
+    List<GameObject> GetAllChildren()
+    {
+        return GetAllChildren(gameObject);
+    }
+
+    List<GameObject> GetAllChildren(GameObject obj)
+    {
+        List<GameObject> children = new List<GameObject>();
+        foreach (Transform child in obj.transform)
+        {
+            children.Add(child.gameObject);
+            children.AddRange(GetAllChildren(child.gameObject));
+        }
+        return children;
+    }
+
+    void Start()
+    {
+        _children = GetAllChildren();
+    }
+}
